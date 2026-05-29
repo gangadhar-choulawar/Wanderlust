f41bb4b69e989c4e59a7d0d1b2c9cee52a98d3fb
gangadhar choulawar <ch.gangadhar2005@gmail.com>
2026-05-29 22:27:15 +0530

    debug

--- app.js
+++ app.js
@@ -1,68 +1,15 @@
-if(process.env.NODE_ENV != "production"){ require('dotenv').config(); }
-
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const path = require("path");
-const methodOverride = require("method-override");
-const ejsMate = require("ejs-mate");
-const ExpressError = require("./utils/ExpressError.js");
-const session = require("express-session");
-const MongoStore = require("connect-mongo");
-const flash = require("connect-flash");
-const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const User = require("./models/user.js");
+console.log("DEBUG: Attempting to load routers...");
 
 const listingRouter = require("./routes/listing.js");
+console.log("DEBUG: Listing Router loaded. Type:", typeof listingRouter);
+
 const reviewRouter = require("./routes/review.js");
+console.log("DEBUG: Review Router loaded. Type:", typeof reviewRouter);
+
 const userRouter = require("./routes/user.js");
+console.log("DEBUG: User Router loaded. Type:", typeof userRouter);
 
-const dbUrl = process.env.ATLASDB_URL;
-async function main() { await mongoose.connect(dbUrl); }
-main().then(() => console.log("connected to DB")).catch(err => console.log(err));
-
-app.engine('ejs', ejsMate);
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname, "/public")));
-
-const store = MongoStore.create({
-    mongoUrl: dbUrl,
-    crypto: { secret: process.env.SECRET },
-    touchAfter: 24 * 3600,
-});
-
-app.use(session({ store, secret: process.env.SECRET, resave: false, saveUninitialized: true, cookie: { expires: Date.now() + 7 * 24 * 60 * 60 * 1000, maxAge: 7 * 24 * 60 * 60 * 1000, httpOnly: true }}));
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-app.use((req, res, next) => {
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
-    next();
-});
-
-app.get("/", (req, res) => { res.redirect("/listings"); });
-
-// ROUTER SECTION - IF IT CRASHES, CHECK EXPORTS IN THE ROUTER FILES
-app.use("/listings", listingRouter);
-app.use("/listings/:id/reviews", reviewRouter);
-app.use("/", userRouter);
-
-app.all(/(.*)/, (req, res, next) => { next(new ExpressError(404, "Page Not Found!")); });
-app.use((err, req, res, next) => {
-    let { statusCode = 500, message = "Something went wrong!" } = err;
-    res.status(statusCode).send(message);
-});
-
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
+// Only use if they are actually functions
+if (typeof listingRouter === 'function') app.use("/listings", listingRouter);
+if (typeof reviewRouter === 'function') app.use("/listings/:id/reviews", reviewRouter);
+if (typeof userRouter === 'function') app.use("/", userRouter);
